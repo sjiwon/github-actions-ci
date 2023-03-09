05feb76a267edc14831b7d8a46a9183c3c44c4e9
SeoJiWon <sjiwon4491@gmail.com>
2023-03-09 15:39:07 +0900

    test: 전체 멤버 조회 테스트

---
+++ src/test/java/com/sjiwon/ci/member/domain/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.sjiwon.ci.member.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.save(Member.createMember("memberA", 20));
+        memberRepository.save(Member.createMember("memberB", 20));
+        memberRepository.save(Member.createMember("memberC", 20));
+    }
+
+    @Test
+    @DisplayName("전체 멤버를 조회한다")
+    void findAllMember() {
+        // when
+        List<Member> members = memberRepository.findAll();
+
+        // then
+        assertAll(
+                () -> assertThat(members.size()).isEqualTo(3),
+                () -> assertThat(members)
+                        .extracting("name")
+                        .contains("memberA", "memberB", "memberC")
+        );
+    }
+}
